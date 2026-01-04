6fd35b003ae17288a31006b66dd08385a3366411
Lucas-Lopes-Alves <lucas.lopes7708@gmail.com>
2026-01-04 15:39:07 -0300

    função de remover tarefas parcialmente adicionada

--- tarefas.cpp
+++ tarefas.cpp
@@ -2,56 +2,83 @@
 #include <fstream> //for data manipulation in file
 #include <string> //to get an entire line from cin
 #include <filesystem> //for std::filesystem::exists
+#include <vector>
 using std::string, std::cin, std::cout;
 
 int main() {   
     int resposta {}; //variavel criada para salvar a resposta e definir o que fazer
-    std::string tarefa; //serve para pegar a tarefa a ser adicionada para colocar no arquivo
+    string tarefa; //serve para pegar a tarefa a ser adicionada para colocar no arquivo
+    std::vector<string> linhas;
     
 
     while (true) //cria o loop que permite usar constantemente
     {
         //verifica se o arquivo das tarefas existe
         std::fstream file;
-        if (std::filesystem::exists("tarefas.txt")) {
-            file.open("tarefas.txt", std::ios::app);
+        if (std::filesystem::exists("/home/lucas/desktop/C/tarefas.txt")) {
+            file.open("/home/lucas/desktop/C/tarefas.txt", std::ios::in | std::ios::app);
         } else {
-            file.open("tarefas.txt", std::ios::out | std::ios::app);
+            file.open("/home/lucas/desktop/C/tarefas.txt", std::ios::in | std::ios::out | std::ios::app);
         }
 
         //pergunta o que o usúario deseja fazer e faz algo com base no número obtido
-        std::cout << "O que deseja fazer?" << '\n'
-                  << "Adicionar tarefa(1) | Remover tarefa (2) | Ver tarefas(3) |Sair(4)" << '\n';
-        std::cin >> resposta;
+        cout << "O que deseja fazer?" << '\n'
+                  << "Adicionar tarefa(1) | Remover/Substituir tarefa (2) {Em construção} | Ver tarefas(3) | Sair(4)" << '\n';
+        cin >> resposta;
+        cout << '\n';
         if (resposta == 1)
         {
-            std::cout << "Digite a tarefa a ser adicionada" << '\n';
-            std::cin.ignore();
+            //limpa o cin e pega a tarefa a ser adicionada no arquivo
+            cout << "Digite a tarefa a ser adicionada" << '\n';
+            cin.ignore();
             std::getline(std::cin, tarefa);
             file << tarefa << '\n';
         }
         else if (resposta == 2)
         {
-            std::cout << "Que tarefa deseja remover(Digite a tarefa exatamente como aparece abaixo)" 
+            cout << "Que tarefa deseja remover(Digite a tarefa exatamente como aparece abaixo)" 
                         << '\n' << "---------------------------" << '\n';
-            for (std::string tarefas; std::getline(file, tarefas);)
+            string tarefas;
+            string removido;
+            int count{0};
+            while (std::getline(file, tarefas))
             {
-                std::cout << tarefas << '\n';
+                cout << count <<": "<< tarefas << '\n';
+                linhas.push_back(tarefas);
+                count++;
             }
+            cin.ignore();
+            std::getline(cin, removido);
+            count = 0;
+            while (std::getline(file, tarefas))
+            {
+                if (tarefas == removido) {
+                    linhas.erase(linhas.begin() + count);
+                }
+                count++;
+            }
+            
         }
         else if (resposta == 3)
         {
-            for (std::string conteudo; std::getline(file, conteudo);)
+            //imprime cada linha do arquivo
+            string conteudo;
+            cout << "-------------------------------------------------------------------------------" << '\n' ;
+            while (std::getline(file, conteudo))
             {
-                std::cout << '\n' << conteudo << '\n';
+                cout << conteudo << '\n';
             }
+            cout << "-------------------------------------------------------------------------------" << '\n' ;
         }
         else if (resposta == 4)
         {
+            //para o programa
             break;
         }
         else {
-            cout << "Opção inválida" ;
+            //resposta caso seja colocada uma opção inválida
+            cout << '\n' << "Opção inválida" << '\n' <<'\n' ;
+            break;
         }
     }
     
